aa43913c35e10bef4beedb7a98f9304876ba5132
Prof Game <kadomony@gmail.com>
2019-07-02 11:49:49 +0200

    exercices 1 et 2

---
+++ Assets/Sprites/Scripts/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float ForceUp = 500f;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector2 force = new Vector2(0f, ForceUp);
+        collision.GetComponent<Rigidbody2D>().AddForce(force);
+    }
+
+}
---
+++ Assets/Sprites/Scripts/Push.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Push : MonoBehaviour
+{
+    public float forceUp = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 force2D = new Vector2(0f, forceUp);
+        GetComponent<Rigidbody2D>().AddForce(force2D);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
