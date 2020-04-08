37f21ffca9d5c5701766bbc6847f02b43f91df8d
Thomas Stassin <thomas@panopticgame.com>
2020-04-08 11:18:30 +0200

    change script

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -6,12 +6,13 @@ public class Bounce : MonoBehaviour
 {
     public float forceUp = 500f;
     public float forceRight = 100f; 
+    public float abc;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Vector2 force = new Vector2(forceRight, forceUp);
-        collision.GetComponent<Rigidbody2D>().AddForce(force);
+        collision.GetComponent<Rigidbody2D>().AddForce(farce);
     }
 
 }
